da8f063c99c9f1013a99a15db21f4d193aeaf072
bamsumit <bam_sumit@hotmail.com>
2021-11-24 06:37:01 -0800

    Clean up of explicit namespace declaration (#11)
    * Updated readme and tutuorial links.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * clean __init__.py
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    Co-authored-by: Marcus G K Williams <marcus.williams@intel.com>

--- src/lava/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2021 Intel Corporation
-# SPDX-License-Identifier: BSD-3-Clause
-# See: https://spdx.org/licenses/
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except:  # noqa E722
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
