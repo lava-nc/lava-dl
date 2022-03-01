56a0dcdc8b376675c0ca50c0c314e310b1e4b2db
bamsumit <bam_sumit@hotmail.com>
2022-02-28 21:09:00 -0800

    Netx (#30)
    * Updated readme and tutuorial links.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * PilotNet verified in Netx
    
    * PiloteNet verified in NetX. Minor liniting fixes.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Changes to accompany io read and reset api changes.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Pilotnet SDNN tested
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * pilotnet sdnn tutorial
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * PilotNet SDNN notebook
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Removed dataset gt logging
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * fixed sdnn notebook plot label
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Integrated virtualport api to swicth from conv to dense layer
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * dense graded input support added. Cleaned up sdnn notebook
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Refport insconsistency in snn notebook
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Linting fixes. Updated build script
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Changed dates in copyright header
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Readme Updates and review comments fixes
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fixed linting issue
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Cleaned up snn notebook. Fixed issue with lambda on Windows.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fixed test_hdf5 tests to reflect in_layer transform changes
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * final tid bits
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    Co-authored-by: Marcus G K Williams <marcus.williams@intel.com>

---
+++ src/lava/lib/dl/netx/blocks/process.py
@@ -0,0 +1,225 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier:  BSD-3-Clause
+
+"""Composable blocks processes in Lava."""
+
+from typing import Type, Union
+import numpy as np
+import h5py
+from lava.magma.core.process.ports.ports import InPort, OutPort
+from lava.magma.core.process.process import AbstractProcess
+from lava.proc.dense.process import Dense as DenseSynapse
+from lava.proc.conv.process import Conv as ConvSynapse
+
+
+class AbstractBlock(AbstractProcess):
+    """Abstract block definition.
+
+    Parameters
+    ----------
+    shape : tuple or list
+        shape of the block output in (x, y, z) or WHC format.
+    neuron_params : dict
+        dictionary of neuron parameters. Defaults to None.
+    has_graded_input : dict
+        flag for graded spikes at input. Defaults to False.
+    """
+    def __init__(self, **kwargs: Union[dict, tuple, list, int, bool]) -> None:
+        super().__init__(**kwargs)
+        self.shape: tuple = kwargs.pop('shape')
+        self.neuron_params = kwargs.pop('neuron_params', None)
+        self.neuron_process = None
+        if self.neuron_params is not None:
+            self.neuron_process = self.neuron_params.pop('neuron_proc', None)
+        self.neuron = None
+        self.has_graded_input = kwargs.pop('has_graded_input', False)
+        self.has_graded_output = False
+        if 'use_graded_spikes' in self.neuron_params.keys():
+            self.has_graded_output = self.neuron_params['use_graded_spikes']
+
+    def _clean(self) -> None:
+        del self.neuron_params
+        del self.neuron_process
+
+    def _neuron(self, bias: np.ndarray) -> Type[AbstractProcess]:
+        if bias is None:
+            return self.neuron_process(shape=self.shape, **self.neuron_params)
+        else:
+            if len(self.shape) == 1:  # scalar bias value
+                bias_mant = bias
+            elif list(bias.shape) == list(self.shape):
+                # neuron wise bias value
+                bias_mant = bias
+            elif self.shape[-1] == bias.shape[0]:  # channel wise bias value
+                bias_mant = np.zeros(self.shape)
+                bias_mant[:, :] = bias
+            else:
+                raise RuntimeError(
+                    f'Bias of shape {bias.shape} could not be broadcast to '
+                    f'neuron of shape {self.shape}.'
+                )
+            neuron_params = {'shape': self.shape, **self.neuron_params}
+            neuron_params['bias'] = bias_mant.astype(np.int32)
+            return self.neuron_process(**neuron_params)
+
+    @property
+    def block(self) -> str:
+        return str(self.__class__.__name__)
+
+
+class Input(AbstractBlock):
+    """Input layer block.
+
+    Parameters
+    ----------
+    shape : tuple or list
+        shape of the layer block in (x, y, z)/WHC format.
+    neuron_params : dict
+        dictionary of neuron parameters. Defaults to None.
+    transform : fx pointer or lambda
+        input transform to be applied. Defualts to ``lambda x: x``.
+    bias : np.ndarray or None
+        bias of input neuron. None means no bias. Defaults to None.
+    """
+    def __init__(self, **kwargs: Union[dict, tuple, list, int, bool]) -> None:
+        super().__init__(**kwargs)
+        self.transform = kwargs.pop('transform', lambda x: x)
+
+        if 'bias_exp' not in self.neuron_params:
+            self.neuron_params['bias_exp'] = 6
+
+        self.neuron = self._neuron(kwargs.pop('bias', None))
+
+        # The input must be handled through neuron's bias.
+        self.inp = InPort(shape=self.neuron.a_in.shape)
+        self.out = OutPort(shape=self.neuron.s_out.shape)
+        self.neuron.s_out.connect(self.out)
+
+        self._clean()
+
+    def export_hdf5(self, handle: Union[h5py.File, h5py.Group]) -> None:
+        raise NotImplementedError
+
+
+class Dense(AbstractBlock):
+    """Dense layer block.
+
+    Parameters
+    ----------
+    shape : tuple or list
+        shape of the layer block in (x, y, z)/WHC format.
+    neuron_params : dict
+        dictionary of neuron parameters. Defaults to None.
+    weight : np.ndarray
+        synaptic weight.
+    bias : np.ndarray or None
+        bias of neuron. None means no bias. Defaults to None.
+    has_graded_input : dict
+        flag for graded spikes at input. Defaults to False.
+    num_weight_bits : int
+        number of weight bits. Defaults to 8.
+    weight_exponent : int
+        weight exponent value. Defaults to 0.
+    sign_mode : int
+        sign mode of the synapse. Refer to lava.proc.dense documentation for
+        details. Default is 1 meaning mixed mode.
+    """
+    def __init__(self, **kwargs: Union[dict, tuple, list, int, bool]) -> None:
+        super().__init__(**kwargs)
+
+        weight = kwargs.pop('weight')
+        num_weight_bits = kwargs.pop('num_weight_bits', 8)
+        weight_exponent = kwargs.pop('weight_exponent', 0)
+        sign_mode = kwargs.pop('sign_mode', 1)
+
+        graded_spikes_params = {'use_graded_spike': self.has_graded_input}
+
+        self.synapse = DenseSynapse(
+            shape=weight.shape,
+            weights=weight,
+            weight_exp=weight_exponent,
+            num_weight_bits=num_weight_bits,
+            sign_mode=sign_mode,
+            **graded_spikes_params,
+        )
+
+        if self.shape != self.synapse.a_out.shape:
+            raise RuntimeError(
+                f'Expected synapse output shape to be {self.shape[-1]}, '
+                f'found {self.synapse.a_out.shape}.'
+            )
+
+        self.neuron = self._neuron(kwargs.pop('bias', None))
+
+        self.inp = InPort(shape=self.synapse.s_in.shape)
+        self.out = OutPort(shape=self.neuron.s_out.shape)
+        self.inp.connect(self.synapse.s_in)
+        self.synapse.a_out.connect(self.neuron.a_in)
+        self.neuron.s_out.connect(self.out)
+
+        self._clean()
+
+    def export_hdf5(self, handle: Union[h5py.File, h5py.Group]) -> None:
+        raise NotImplementedError
+
+
+class Conv(AbstractBlock):
+    """Conv layer block.
+
+    Parameters
+    ----------
+    shape : tuple or list
+        shape of the layer block in (x, y, z)/WHC format.
+    input_shape : tuple or list
+        shape of input layer in (x, y, z)/WHC format.
+    neuron_params : dict
+        dictionary of neuron parameters. Defaults to None.
+    weight : np.ndarray
+        kernel weight.
+    bias : np.ndarray or None
+        bias of neuron. None means no bias. Defaults to None.
+    stride : int or tuple of ints, optional
+        convolution stride. Defaults to 1.
+    padding : int or tuple of ints, optional
+        convolution padding. Defaults to 0.
+    dilation : int or tuple of ints, optional
+        convolution dilation. Defaults to 1.
+    groups : int
+        convolution groups. Defaults to 1.
+    has_graded_input : dict
+        flag for graded spikes at input. Defaults to False.
+    """
+    def __init__(self, **kwargs: Union[dict, tuple, list, int, bool]) -> None:
+        super().__init__(**kwargs)
+        weight = kwargs.pop('weight')
+
+        graded_spikes_params = {'use_graded_spike': self.has_graded_input}
+
+        self.synapse = ConvSynapse(
+            input_shape=kwargs.pop('input_shape'),
+            weight=weight,
+            stride=kwargs.pop('stride', 1),
+            padding=kwargs.pop('padding', 0),
+            dilation=kwargs.pop('dilation', 1),
+            groups=kwargs.pop('groups', 1),
+            **graded_spikes_params,
+        )
+
+        if list(self.shape) != list(self.synapse.output_shape):
+            raise RuntimeError(
+                f'Expected synapse output shape to be {self.shape}, '
+                f'found {self.synapse.output_shape}.'
+            )
+
+        self.neuron = self._neuron(kwargs.pop('bias', None))
+
+        self.inp = InPort(shape=self.synapse.s_in.shape)
+        self.out = OutPort(shape=self.neuron.s_out.shape)
+        self.inp.connect(self.synapse.s_in)
+        self.synapse.a_out.connect(self.neuron.a_in)
+        self.neuron.s_out.connect(self.out)
+
+        self._clean()
+
+    def export_hdf5(self, handle: Union[h5py.File, h5py.Group]) -> None:
+        raise NotImplementedError
---
+++ src/lava/lib/dl/netx/utils.py
@@ -0,0 +1,172 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier:  BSD-3-Clause
+
+"""HDF5 net description manipulation utilities."""
+
+from typing import Tuple, Union
+import h5py
+import numpy as np
+from enum import IntEnum, unique
+
+
+@unique
+class SYNAPSE_SIGN_MODE(IntEnum):
+    """Enum for synapse sign mode. Options are {``MIXED : 1``,
+    ``EXCITATORY : 2`` and ``INHIBITORY : 2``}.
+    """
+    MIXED = 1
+    EXCITATORY = 2
+    INHIBITORY = 3
+
+
+class NetDict:
+    """Provides dictionary like access to h5py object without the h5py quirks
+
+    Parameters
+    ----------
+    filename : str or None, optional
+        filename of h5py file to be loaded. It is only invoked if hdf5 file
+        handle ``f`` is ``None``. Default is None.
+    mode : str, optional
+        file open mode, by default 'r'.
+    f : h5py.File or h5py.Group, optional
+        hdf5 file object handle. Overwrites the function of filename if it is
+        not ``None``. Default is None.
+    """
+    def __init__(
+        self,
+        filename: Union[str, None] = None,
+        mode: str = 'r',
+        f: Union[h5py.File, h5py.Group] = None,
+    ) -> None:
+        self.f = h5py.File(filename, mode) if f is None else f
+        self.str_keys = ['type']
+        self.array_keys = [
+            'shape', 'stride', 'padding', 'dilation', 'groups', 'delay',
+            'iDecay', 'refDelay', 'scaleRho', 'tauRho', 'theta', 'vDecay',
+            'vThMant', 'wgtExp',
+        ]
+        self.copy_keys = ['weight', 'bias']
+
+    def keys(self) -> h5py._hl.base.KeysViewHDF5:
+        return self.f.keys()
+
+    def __len__(self) -> int:
+        return len(self.f)
+
+    def __getitem__(self, key: str) -> h5py.Dataset:
+        if key in self.str_keys:
+            value = self.f[key]
+            if len(value.shape) > 0:
+                value = value[0]
+            else:
+                value = value[()]
+            return value.decode('ascii')
+        elif key in self.copy_keys:
+            return self.f[key][()].astype(int).copy()
+        elif key in self.array_keys:
+            return self.f[key][()]
+        elif isinstance(key, int) and f'{key}' in self.f.keys():
+            return NetDict(f=self.f[f'{key}'])
+        else:
+            return NetDict(f=self.f[key])
+
+    def __setitem__(self, key: str) -> None:
+        raise NotImplementedError('Set feature is not implemented.')
+
+
+def optimize_weight_bits(weight: np.ndarray) -> Tuple[
+    np.ndarray, int, int, SYNAPSE_SIGN_MODE
+]:
+    """Optimizes the weight matrix to best fit in Loihi's synapse.
+
+    Parameters
+    ----------
+    weight : np.ndarray
+        standard 8 bit signed weight matrix.
+
+    Returns
+    -------
+    np.ndarray
+        optimized weight matrix
+    int
+        weight bits
+    int
+        weight_exponent
+    SYNAPSE_SIGN_MODE
+        synapse sign mode
+    """
+    max_weight = np.max(weight)
+    min_weight = np.min(weight)
+
+    if max_weight < 0:
+        sign_mode = SYNAPSE_SIGN_MODE.INHIBITORY
+        is_signed = 0
+    elif min_weight >= 0:
+        sign_mode = SYNAPSE_SIGN_MODE.EXCITATORY
+        is_signed = 0
+    else:
+        sign_mode = SYNAPSE_SIGN_MODE.MIXED
+        is_signed = 1
+
+    if sign_mode == SYNAPSE_SIGN_MODE.MIXED:
+        pos_scale = 127 / max_weight
+        neg_scale = -128 / min_weight
+        scale = np.min([pos_scale, neg_scale])
+    elif sign_mode == SYNAPSE_SIGN_MODE.INHIBITORY:
+        scale = -256 / min_weight
+    elif sign_mode == SYNAPSE_SIGN_MODE.EXCITATORY:
+        scale = 255 / max_weight
+
+    scale_bits = np.floor(np.log2(scale)) + is_signed
+
+    precision_found = False
+    n = 8
+    while (precision_found is False) and (n > 0):
+        roundingError = np.sum(
+            np.abs(weight / (2**n) - np.round(weight / (2**n)))
+        )
+        if roundingError == 0:
+            precision_found = True
+        else:
+            n -= 1
+
+    n -= is_signed
+
+    num_weight_bits = 8 - scale_bits - n
+    weight_exponent = -scale_bits
+
+    weight = np.left_shift(weight.astype(np.int32), int(scale_bits))
+
+    return (
+        weight.astype(int),
+        int(num_weight_bits),
+        int(weight_exponent),
+        sign_mode
+    )
+
+
+def num_delay_bits(delays: np.ndarray) -> int:
+    """Calculates the number of delay bits required.
+
+    Parameters
+    ----------
+    delays : np.ndarray
+        delay vector
+
+    Returns
+    -------
+    int
+        number of delay bits.
+    """
+    if delays.min() < 0:
+        raise ValueError(
+            f'Negative delay encountered. '
+            f'Found {delays.min()=}.'
+        )
+    if delays.max() >= 63:
+        raise ValueError(
+            f'Max delay exceeded limit of 62. '
+            f'Found {delays.max()=}.'
+        )
+    return np.ceil(np.log2(delays.max())).astype(int)
