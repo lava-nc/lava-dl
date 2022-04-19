2e616934a596fc3bd23f2c87a48d1c69a58cd8e6
Marcus G K Williams <168222+mgkwill@users.noreply.github.com>
2022-04-19 08:46:45 -0700

    Update utils.py
    Updating date.

--- tutorials/lava/lib/dl/slayer/pilotnet/utils.py
+++ tutorials/lava/lib/dl/slayer/pilotnet/utils.py
@@ -1,4 +1,4 @@
-# Copyright © 2021 Intel Corporation.
+# Copyright © 2022 Intel Corporation.
 # 
 # This software and the related documents are Intel copyrighted
 # materials, and your use of them is governed by the express 
