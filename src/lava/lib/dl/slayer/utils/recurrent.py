8b5b29eeecf7e392e95249e5d7b7544bef92f1a0
Jonathan Timcheck <jonathan.timcheck@intel.com>
2022-09-16 18:37:09 +0000

    lint
    Signed-off-by: Jonathan Timcheck <jonathan.timcheck@intel.com>

--- src/lava/lib/dl/slayer/utils/recurrent.py
+++ src/lava/lib/dl/slayer/utils/recurrent.py
@@ -1,40 +1,42 @@
-from syslog import LOG_CONS
 import torch
 
 
 # dendrite_print_time_index = -1
 # feedback_print_time_index = -1
 spike_print_time_index = 48
+
+
 def custom_recurrent_ground_truth_1(z, neuron, recurrent_synapse):
     log = []
     # x = torch.zeros_like(z).to(x.device)
-    print('custom_recurrent_ground_truth_1')
+    print("custom_recurrent_ground_truth_1")
     x = torch.zeros_like(z).to(z.device)
     spike = torch.zeros(z.shape[:-1]).to(x.device)
     for time in range(z.shape[-1]):
         log_item = {}
-        dendrite = z[..., time:time + 1]
-        log_item['dendrite'] = dendrite
+        dendrite = z[..., time: time + 1]
+        log_item["dendrite"] = dendrite
         # if time == dendrite_print_time_index:
         #     print(f'{dendrite.shape=}')
         #     print(f'{dendrite.squeeze()=}')
         feedback = recurrent_synapse(spike.reshape(dendrite.shape))
-        log_item['feedback'] = feedback
+        log_item["feedback"] = feedback
         # if time == feedback_print_time_index:
         #     print(f'{feedback.shape=}')
         #     print(f'{feedback.squeeze()=}')
         spike = neuron(dendrite + feedback)
-        log_item['spike'] = spike
+        log_item["spike"] = spike
         if time == spike_print_time_index:
-            print(f'{spike.shape=}')
-            print(f'{spike.squeeze()=}')
-        x[..., time:time + 1] = spike
+            print(f"{spike.shape=}")
+            print(f"{spike.squeeze()=}")
+        x[..., time: time + 1] = spike
         log.append(log_item)
     return x, log
 
+
 def custom_recurrent_ground_truth_2(z, neuron, recurrent_synapse):
     log = []
-    print('custom_recurrent_ground_truth_2')
+    print("custom_recurrent_ground_truth_2")
     x = torch.zeros_like(z).to(z.device)
     mat_shape = recurrent_synapse.weight.shape[:2]
     pre_hook = recurrent_synapse.pre_hook_fx
@@ -47,23 +49,24 @@ def custom_recurrent_ground_truth_2(z, neuron, recurrent_synapse):
     for time in range(z.shape[-1]):
         log_item = {}
         dendrite = z[..., time]
-        log_item['dendrite'] = dendrite
+        log_item["dendrite"] = dendrite
         # if time == dendrite_print_time_index:
         #     print(f'{dendrite.shape=}')
         #     print(f'{dendrite.squeeze()=}')
         feedback = torch.matmul(spike[..., 0], recurrent_mat_T)
-        log_item['feedback'] = feedback
+        log_item["feedback"] = feedback
         # if time == feedback_print_time_index:
         #     print(f'{feedback.shape=}')
         #     print(f'{feedback.squeeze()=}')
         spike = neuron(torch.unsqueeze(dendrite + feedback, dim=-1))
-        log_item['spike'] = spike
+        log_item["spike"] = spike
         if time == spike_print_time_index:
-            print(f'{spike.shape=}')
-            print(f'{spike.squeeze()=}')
-        x[..., time:time + 1] = spike
+            print(f"{spike.shape=}")
+            print(f"{spike.squeeze()=}")
+        x[..., time: time + 1] = spike
         log.append(log_item)
     return x, log
+
 
 def custom_recurrent(z, neuron, recurrent_synapse):
     mat_shape = recurrent_synapse.weight.shape[:2]
@@ -78,13 +81,13 @@ def custom_recurrent(z, neuron, recurrent_synapse):
 class CustomRecurrent(torch.autograd.Function):
     @staticmethod
     def forward(ctx, z, neuron, recurrent_mat):
-        '''
-                        ------- R <------
-         fb[t] = R s[t] |               |
-                        v               |
-             z[t]----->(+)---->( N )----|----> s[t]
+        """
+                       ------- R <------
+        fb[t] = R s[t] |               |
+                       v               |
+            z[t]----->(+)---->( N )----|----> s[t]
 
-        '''
+        """
         z = z.detach().requires_grad_()
         x = torch.zeros_like(z).to(z.device)
         recurrent_mat_T = recurrent_mat.transpose(0, 1).clone().detach()
@@ -100,7 +103,7 @@ class CustomRecurrent(torch.autograd.Function):
                 spike = neuron(torch.unsqueeze(dend_sum, dim=-1))
                 ctx.dend_sums.append(dend_sum)
                 ctx.spikes.append(spike)
-            x[..., time:time + 1] = spike
+            x[..., time: time + 1] = spike
 
         ctx.recurrent_mat = recurrent_mat
         ctx.x = x
@@ -108,40 +111,43 @@ class CustomRecurrent(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_x):
-        '''
-                        ------> R -------
-                        |               | grad_fb = (R.T) d/dz[t]
-                        |               v
-            d/dz[t]<----|----( N )<----(+)---- d/ds[t]
-                    grad_dend
-        '''
+        """
+                    ------> R -------
+                    |               | grad_fb = (R.T) d/dz[t]
+                    |               v
+        d/dz[t]<----|----( N )<----(+)---- d/ds[t]
+                grad_dend
+        """
         grad_z = torch.zeros_like(grad_x).to(grad_x.device)
         grad_neuron = None
         grad_spike = 0
         # grad_recurrent_mat = torch.zeros_like(ctx.recurrent_mat).to(grad_x.device)
         for time in range(grad_x.shape[-1])[::-1]:
-            grad_spike = grad_spike + grad_x[..., time:time + 1]
+            grad_spike = grad_spike + grad_x[..., time: time + 1]
             torch.autograd.backward(ctx.spikes[time], grad_spike)
             grad_dend_sum = ctx.dend_sums[time].grad
             grad_feedback = grad_dend_sum
             grad_dendrite = grad_dend_sum
             # if time >= 1:
-            #     grad_recurrent_mat += torch.matmul(grad_feedback.transpose(0, 1), ctx.spikes[time - 1][..., 0])
-            grad_spike = torch.unsqueeze(torch.matmul(grad_feedback,
-                                                      ctx.recurrent_mat),
-                                         dim=-1)
+            #     grad_recurrent_mat += 
+            #     torch.matmul(grad_feedback.transpose(0, 1),
+            #     ctx.spikes[time - 1][..., 0])
+            grad_spike = torch.unsqueeze(
+                torch.matmul(grad_feedback, ctx.recurrent_mat), dim=-1
+            )
             grad_z[..., time] = grad_dendrite
 
         # Factoring the recurrent gradient computation out of for loop
         # gradW = grad_output x input = grad_feedback x spike
         # grad_output: N, C_out, T => C_out, NT
         # input: N, C_in, T => NT, C_in
-        grad_output = grad_z[..., 1:].transpose(
-            0, 1).reshape(grad_dendrite.shape[1], -1)
+        grad_output = (
+            grad_z[..., 1:].transpose(0, 1).reshape(grad_dendrite.shape[1], -1)
+        )
         input = ctx.x[..., :-1].transpose(1, 2).reshape(-1, ctx.x.shape[1])
-        # grad_output = grad_z[..., 1:].permute([1, 0, 2]).reshape(grad_dendrite.shape[1], -1)
+        # grad_output = 
+        # grad_z[..., 1:].permute([1, 0, 2]).reshape(grad_dendrite.shape[1], -1)
         # input = ctx.x[..., :-1].permute([0, 2, 1]).reshape(-1, ctx.x.shape[1])
         grad_recurrent_mat = torch.matmul(grad_output, input)
 
         return grad_z, grad_neuron, grad_recurrent_mat
-
--- tests/lava/lib/dl/slayer/utils/test_recurrent.py
+++ tests/lava/lib/dl/slayer/utils/test_recurrent.py
@@ -2,26 +2,30 @@ import sys
 import unittest
 import torch
 
-from src.lava.lib.dl.slayer.utils.recurrent import custom_recurrent, custom_recurrent_ground_truth_1, custom_recurrent_ground_truth_2
+from src.lava.lib.dl.slayer.utils.recurrent import (
+    custom_recurrent,
+    custom_recurrent_ground_truth_1,
+    custom_recurrent_ground_truth_2,
+)
 from src.lava.lib.dl.slayer.neuron.cuba import Neuron
 from src.lava.lib.dl.slayer.synapse.layer import Dense
 
 import itertools
 
-verbose = True if (('-v' in sys.argv) or ('--verbose' in sys.argv)) else False
+verbose = True if (("-v" in sys.argv) or ("--verbose" in sys.argv)) else False
+
 
 class TestRecurrent(unittest.TestCase):
     def test_recurrent(self):
 
         if verbose is True:
-            print('testing recurrent')
+            print("testing recurrent")
 
         torch.manual_seed(298101)
 
         batch_size = 2
         n_time_steps = 1024
         n_neur = 100
-
 
         # batch_size = 3
         # n_time_steps = 10
@@ -31,140 +35,225 @@ class TestRecurrent(unittest.TestCase):
         # n_time_steps = 1024
         # n_neur = 256
 
+        device = torch.device("cuda")
 
+        cuba_params = {
+            "threshold": 1.25,
+            "current_decay": 0.25,
+            "voltage_decay": 0.25,
+            "shared_param": True,
+            "requires_grad": True,
+            "graded_spike": False,
+        }
 
-        device = torch.device('cuda')
-
-        cuba_params = {'threshold': 1.25,
-                'current_decay': 0.25,
-                'voltage_decay': 0.25,
-                'shared_param': True,
-                'requires_grad': True,
-                'graded_spike': False}
-
-        z = torch.randn((batch_size, n_neur, n_time_steps,)).to(device)
+        z = torch.randn(
+            (
+                batch_size,
+                n_neur,
+                n_time_steps,
+            )
+        ).to(device)
         if verbose is True:
-            print(f'{z.shape=}')
+            print(f"{z.shape=}")
 
-        # Create three independent neuron and recurrent_syanpse - one for each version of custom_recurrent
-        neuron_list = list(map(lambda _: Neuron(**cuba_params, persistent_state=True).to(device), range(3)))
-        recurrent_synapse_list = list(map(lambda _: Dense(in_neurons=n_neur, out_neurons=n_neur).to(device), range(3)))
-        recurrent_synapse_list[1].weight.data = recurrent_synapse_list[0].weight.data.detach().clone()
-        recurrent_synapse_list[2].weight.data = recurrent_synapse_list[0].weight.data.detach().clone()
+        # Create three independent neuron and recurrent_syanpse - one for each version
+        # of custom_recurrent
+        neuron_list = list(
+            map(
+                lambda _: Neuron(**cuba_params, persistent_state=True).to(
+                    device
+                ),
+                range(3),
+            )
+        )
+        recurrent_synapse_list = list(
+            map(
+                lambda _: Dense(in_neurons=n_neur, out_neurons=n_neur).to(
+                    device
+                ),
+                range(3),
+            )
+        )
+        recurrent_synapse_list[1].weight.data = (
+            recurrent_synapse_list[0].weight.data.detach().clone()
+        )
+        recurrent_synapse_list[2].weight.data = (
+            recurrent_synapse_list[0].weight.data.detach().clone()
+        )
 
-        for recurrent_synapse, neuron in zip(recurrent_synapse_list, neuron_list):
+        for recurrent_synapse, neuron in zip(
+            recurrent_synapse_list, neuron_list
+        ):
             recurrent_synapse.pre_hook_fx = neuron.quantize_8bit
 
-        list(map(lambda i : print(f'recurrent_synapse_list[{i}].pre_hook_fx={recurrent_synapse_list[i].pre_hook_fx}'), range(3)))
+        list(
+            map(
+                lambda i: print(
+                    f"recurrent_synapse_list[{i}].pre_hook_fx"
+                    + "={recurrent_synapse_list[i].pre_hook_fx}"
+                ),
+                range(3),
+            )
+        )
 
         # import pdb; pdb.set_trace()
         # recurrent_synapse_list[0].
 
-        custom_recurrent_output = custom_recurrent(z, neuron_list[0], recurrent_synapse_list[0])
-        custom_recurrent_ground_truth_1_output, log_1 = custom_recurrent_ground_truth_1(z, neuron_list[1], recurrent_synapse_list[1])
-        custom_recurrent_ground_truth_2_output, log_2 = custom_recurrent_ground_truth_2(z, neuron_list[2], recurrent_synapse_list[2])
+        custom_recurrent_output = custom_recurrent(
+            z, neuron_list[0], recurrent_synapse_list[0]
+        )
+        (
+            custom_recurrent_ground_truth_1_output,
+            log_1,
+        ) = custom_recurrent_ground_truth_1(
+            z, neuron_list[1], recurrent_synapse_list[1]
+        )
+        (
+            custom_recurrent_ground_truth_2_output,
+            log_2,
+        ) = custom_recurrent_ground_truth_2(
+            z, neuron_list[2], recurrent_synapse_list[2]
+        )
 
-        forward_error_1 = torch.norm(custom_recurrent_output - custom_recurrent_ground_truth_1_output).item()
-        forward_error_2 = torch.norm(custom_recurrent_output - custom_recurrent_ground_truth_2_output).item()
-        forward_error_1_2 = torch.norm(custom_recurrent_output - custom_recurrent_ground_truth_2_output).item()
+        forward_error_1 = torch.norm(
+            custom_recurrent_output - custom_recurrent_ground_truth_1_output
+        ).item()
+        forward_error_2 = torch.norm(
+            custom_recurrent_output - custom_recurrent_ground_truth_2_output
+        ).item()
+        forward_error_1_2 = torch.norm(
+            custom_recurrent_output - custom_recurrent_ground_truth_2_output
+        ).item()
         if verbose is True:
-            print(f'Forward Error 1: {forward_error_1}')
-            print(f'Forward Error 2: {forward_error_2}')
-            print(f'Forward Error 1-2: {forward_error_1_2}')
-
-
+            print(f"Forward Error 1: {forward_error_1}")
+            print(f"Forward Error 2: {forward_error_2}")
+            print(f"Forward Error 1-2: {forward_error_1_2}")
 
         # Use the same random gradient at output and perform backprop from there
         grad_output = torch.rand_like(custom_recurrent_output)
         torch.autograd.backward(custom_recurrent_output, grad_output)
-        torch.autograd.backward(custom_recurrent_ground_truth_1_output, grad_output)
-        torch.autograd.backward(custom_recurrent_ground_truth_2_output, grad_output)
+        torch.autograd.backward(
+            custom_recurrent_ground_truth_1_output, grad_output
+        )
+        torch.autograd.backward(
+            custom_recurrent_ground_truth_2_output, grad_output
+        )
 
-        recurrent_weight_error_1 = torch.norm(recurrent_synapse_list[0].weight.grad
-                                        - recurrent_synapse_list[1].weight.grad).item()
-        recurrent_weight_error_2 = torch.norm(recurrent_synapse_list[0].weight.grad
-                                        - recurrent_synapse_list[2].weight.grad).item()
-        recurrent_weight_error_1_2 = torch.norm(recurrent_synapse_list[1].weight.grad
-                                        - recurrent_synapse_list[2].weight.grad).item()
-   
+        recurrent_weight_error_1 = torch.norm(
+            recurrent_synapse_list[0].weight.grad
+            - recurrent_synapse_list[1].weight.grad
+        ).item()
+        recurrent_weight_error_2 = torch.norm(
+            recurrent_synapse_list[0].weight.grad
+            - recurrent_synapse_list[2].weight.grad
+        ).item()
+        recurrent_weight_error_1_2 = torch.norm(
+            recurrent_synapse_list[1].weight.grad
+            - recurrent_synapse_list[2].weight.grad
+        ).item()
+
         if verbose is True:
-            print(f'Recurrent Weight Grad Error 1: {recurrent_weight_error_1}')
-            print(f'Recurrent Weight Grad Error 2: {recurrent_weight_error_2}')
-            print(f'Recurrent Weight Grad Error 1-2: {recurrent_weight_error_1_2}')
+            print(f"Recurrent Weight Grad Error 1: {recurrent_weight_error_1}")
+            print(f"Recurrent Weight Grad Error 2: {recurrent_weight_error_2}")
+            print(
+                f"Recurrent Weight Grad Error 1-2: {recurrent_weight_error_1_2}"
+            )
 
-
-
-
-
-        current_decay_error_1 = torch.norm(neuron_list[0].current_decay.grad
-                                     - neuron_list[1].current_decay.grad).item()
-        current_decay_error_2 = torch.norm(neuron_list[0].current_decay.grad
-                                     - neuron_list[2].current_decay.grad).item()
+        current_decay_error_1 = torch.norm(
+            neuron_list[0].current_decay.grad
+            - neuron_list[1].current_decay.grad
+        ).item()
+        current_decay_error_2 = torch.norm(
+            neuron_list[0].current_decay.grad
+            - neuron_list[2].current_decay.grad
+        ).item()
         if verbose is True:
-            print(f'Current Decay Grad Error 1: {current_decay_error_1}')
-            print(f'Current Decay Grad Error 2: {current_decay_error_2}')
+            print(f"Current Decay Grad Error 1: {current_decay_error_1}")
+            print(f"Current Decay Grad Error 2: {current_decay_error_2}")
 
-
-
-        voltage_decay_error_1 = torch.norm(neuron_list[0].voltage_decay.grad
-                                     - neuron_list[1].voltage_decay.grad).item()
-        voltage_decay_error_2 = torch.norm(neuron_list[0].voltage_decay.grad
-                                     - neuron_list[2].voltage_decay.grad).item()
+        voltage_decay_error_1 = torch.norm(
+            neuron_list[0].voltage_decay.grad
+            - neuron_list[1].voltage_decay.grad
+        ).item()
+        voltage_decay_error_2 = torch.norm(
+            neuron_list[0].voltage_decay.grad
+            - neuron_list[2].voltage_decay.grad
+        ).item()
         if verbose is True:
-            print(f'Voltage Decay Grad Error 1: {voltage_decay_error_1}')
-            print(f'Voltage Decay Grad Error 2: {voltage_decay_error_2}')
-
-
+            print(f"Voltage Decay Grad Error 1: {voltage_decay_error_1}")
+            print(f"Voltage Decay Grad Error 2: {voltage_decay_error_2}")
 
         # examine logs
-        dendrite_norms, feedback_norms, spike_norms = map(lambda key: (
-            list(map(lambda log_item_1, log_item_2, i: (i, torch.norm(log_item_1[key].squeeze() - log_item_2[key].squeeze() ).item()), log_1, log_2, itertools.count()))
-        ), ('dendrite', 'feedback', 'spike'))
+        dendrite_norms, feedback_norms, spike_norms = map(
+            lambda key: (
+                list(
+                    map(
+                        lambda log_item_1, log_item_2, i: (
+                            i,
+                            torch.norm(
+                                log_item_1[key].squeeze()
+                                - log_item_2[key].squeeze()
+                            ).item(),
+                        ),
+                        log_1,
+                        log_2,
+                        itertools.count(),
+                    )
+                )
+            ),
+            ("dendrite", "feedback", "spike"),
+        )
 
-
-        print(f'{dendrite_norms=}')
-        print(f'{feedback_norms=}')
-        print(f'{spike_norms=}')
+        print(f"{dendrite_norms=}")
+        print(f"{feedback_norms=}")
+        print(f"{spike_norms=}")
         # import pdb; pdb.set_trace()
         # custom_recurrent_ground_truth_2(z, neuron_list[0], recurrent_synapse_list[0])
 
         epsilon = 1e-3
         self.assertTrue(
             forward_error_1 < epsilon,
-            f'Error in recurrent. Expected forward_error_1<{epsilon}. Found {forward_error_1=}.'
+            "Error in recurrent. Expected "
+            + f"forward_error_1<{epsilon}. Found {forward_error_1=}.",
         )
         self.assertTrue(
             forward_error_2 < epsilon,
-            f'Error in recurrent. Expected forward_error_2<{epsilon}. Found {forward_error_2=}.'
+            "Error in recurrent. Expected "
+            + f"forward_error_2<{epsilon}. Found {forward_error_2=}.",
         )
 
         self.assertTrue(
             recurrent_weight_error_1 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_1<{epsilon}. Found {recurrent_weight_error_1=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_1<{epsilon}. Found {recurrent_weight_error_1=}.",
         )
         self.assertTrue(
             recurrent_weight_error_2 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_2<{epsilon}. Found {recurrent_weight_error_2=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_2<{epsilon}. Found {recurrent_weight_error_2=}.",
         )
-      
+
         self.assertTrue(
             current_decay_error_1 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_1<{epsilon}. Found {current_decay_error_1=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_1<{epsilon}. Found {current_decay_error_1=}.",
         )
         self.assertTrue(
             current_decay_error_2 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_1<{epsilon}. Found {current_decay_error_2=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_1<{epsilon}. Found {current_decay_error_2=}.",
         )
 
         self.assertTrue(
             voltage_decay_error_1 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_1<{epsilon}. Found {voltage_decay_error_1=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_1<{epsilon}. Found {voltage_decay_error_1=}.",
         )
         self.assertTrue(
             voltage_decay_error_2 < epsilon,
-            f'Error in recurrent. Expected recurrent_weight_error_2<{epsilon}. Found {voltage_decay_error_2=}.'
+            "Error in recurrent. Expected "
+            + f"recurrent_weight_error_2<{epsilon}. Found {voltage_decay_error_2=}.",
         )
-      
 
         # if error > 1e-6:
         #     raise Exception(
