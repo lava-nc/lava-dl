0bbb7887c253f31f81be0d627d2f957762cd253d
Alexander Henkes <alexanderhenkes1@gmail.com>
2023-08-12 15:06:24 +0200

    E501.
    The test file failed with message:
    
        'test_cuba.py:19:80: E501 line too long (80 > 79 characters)'
    
    Fixed by reformatting.

--- tests/lava/lib/dl/slayer/block/test_cuba.py
+++ tests/lava/lib/dl/slayer/block/test_cuba.py
@@ -15,8 +15,9 @@ from lava.magma.core.run_conditions import RunSteps
 from lava.proc.conv import utils
 from lava.proc import io
 
-verbose = True if (('-v' in sys.argv) or ('--verbose' in sys.argv)) else False
-# Enabling torch sometimes causes multiprocessing error, especially in unittests
+verbose = True if (("-v" in sys.argv) or ("--verbose" in sys.argv)) else False
+# Enabling torch sometimes causes multiprocessing error, 
+# especially in unittests
 utils.TORCH_IS_AVAILABLE = False
 
 # seed = np.random.randint(1000)
@@ -25,22 +26,22 @@ np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 if verbose:
-    print(f'{seed=}')
+    print(f"{seed=}")
 
 if torch.cuda.is_available():
-    device = torch.device('cuda')
+    device = torch.device("cuda")
 else:
     if verbose:
-        print('CUDA is not available in the system. '
-              'Testing for CPU version only.')
-    device = torch.device('cpu')
+        print(
+            "CUDA is not available in the system. "
+            "Testing for CPU version only."
+        )
+    device = torch.device("cpu")
 
-tempdir = os.path.dirname(__file__) + '/temp'
+tempdir = os.path.dirname(__file__) + "/temp"
 os.makedirs(tempdir, exist_ok=True)
 
-neuron_param = {'threshold': 0.5,
-                'current_decay': 0.5,
-                'voltage_decay': 0.5}
+neuron_param = {"threshold": 0.5, "current_decay": 0.5, "voltage_decay": 0.5}
 
 
 class TestCUBA(unittest.TestCase):
@@ -58,17 +59,18 @@ class TestCUBA(unittest.TestCase):
         y = net(x)
 
         # export slayer network
-        net.export_hdf5(h5py.File(tempdir + '/cuba_dense.net',
-                                  'w').create_group('layer/0'))
+        net.export_hdf5(
+            h5py.File(tempdir + "/cuba_dense.net", "w").create_group("layer/0")
+        )
 
         # create equivalent lava network using netx and evaluate output
-        lava_net = netx.hdf5.Network(net_config=tempdir + '/cuba_dense.net')
+        lava_net = netx.hdf5.Network(net_config=tempdir + "/cuba_dense.net")
         source = io.source.RingBuffer(data=x[0])
         sink = io.sink.RingBuffer(shape=lava_net.out.shape, buffer=time_steps)
         source.s_out.connect(lava_net.inp)
         lava_net.out.connect(sink.a_in)
         run_condition = RunSteps(num_steps=time_steps)
-        run_config = Loihi1SimCfg(select_tag='fixed_pt')
+        run_config = Loihi1SimCfg(select_tag="fixed_pt")
         lava_net.run(condition=run_condition, run_cfg=run_config)
         output = sink.data.get()
         lava_net.stop()
@@ -76,9 +78,9 @@ class TestCUBA(unittest.TestCase):
         if verbose:
             print()
             print(lava_net)
-            print('slayer output:')
+            print("slayer output:")
             print(y[0])
-            print('lava output:')
+            print("lava output:")
             print(output)
 
         self.assertTrue(np.abs(y[0].data.numpy() - output).sum() == 0)
@@ -93,25 +95,30 @@ class TestCUBA(unittest.TestCase):
         time_steps = 10
 
         # create slayer network and evaluate output
-        net = slayer.block.cuba.Conv(neuron_param,
-                                     in_features, out_features, kernel_size)
-        x = (torch.rand([1, in_features,
-                         height, width, time_steps]) > 0.5).float()
+        net = slayer.block.cuba.Conv(
+            neuron_param, in_features, out_features, kernel_size
+        )
+        x = (
+            torch.rand([1, in_features, height, width, time_steps]) > 0.5
+        ).float()
         y = net(x).permute((0, 3, 2, 1, 4))
 
         # export slayer network
-        net.export_hdf5(h5py.File(tempdir + '/cuba_conv.net',
-                                  'w').create_group('layer/0'))
+        net.export_hdf5(
+            h5py.File(tempdir + "/cuba_conv.net", "w").create_group("layer/0")
+        )
 
         # create equivalent lava network using netx and evaluate output
-        lava_net = netx.hdf5.Network(net_config=tempdir + '/cuba_conv.net',
-                                     input_shape=(width, height, in_features))
+        lava_net = netx.hdf5.Network(
+            net_config=tempdir + "/cuba_conv.net",
+            input_shape=(width, height, in_features),
+        )
         source = io.source.RingBuffer(data=x[0].permute((2, 1, 0, 3)))
         sink = io.sink.RingBuffer(shape=lava_net.out.shape, buffer=time_steps)
         source.s_out.connect(lava_net.inp)
         lava_net.out.connect(sink.a_in)
         run_condition = RunSteps(num_steps=time_steps)
-        run_config = Loihi1SimCfg(select_tag='fixed_pt')
+        run_config = Loihi1SimCfg(select_tag="fixed_pt")
         lava_net.run(condition=run_condition, run_cfg=run_config)
         output = sink.data.get()
         lava_net.stop()
@@ -119,9 +126,9 @@ class TestCUBA(unittest.TestCase):
         if verbose:
             print()
             print(lava_net)
-            print('slayer output:')
+            print("slayer output:")
             print(y[0][0, 0, 0])
-            print('lava output:')
+            print("lava output:")
             print(output[0, 0, 0])
 
         self.assertTrue(np.abs(y[0].data.numpy() - output).sum() == 0)
