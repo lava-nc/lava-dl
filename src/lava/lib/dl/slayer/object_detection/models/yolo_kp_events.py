6f96b128d6f196e024fbf2bb4cb73f526aa79e10
y <leobardo.e.campos.macias@intel.com>
2023-12-01 08:27:55 -0800

    tunning
    Signed-off-by: y <leobardo.e.campos.macias@intel.com>

--- src/lava/lib/dl/slayer/object_detection/models/yolo_kp_events.py
+++ src/lava/lib/dl/slayer/object_detection/models/yolo_kp_events.py
@@ -90,8 +90,8 @@ class Network(YOLOBase):
         #])
 
         self.blocks = torch.nn.ModuleList([
-            slayer.block.cuba.Conv(neuron_cuba_kwargs,   2,  16, 3, padding=1, stride=2, weight_scale=3, **block_8_kwargs),
-            slayer.block.cuba.Conv(neuron_cuba_kwargs,  16,  32, 3, padding=1, stride=2, weight_scale=3, **block_8_kwargs),
+            slayer.block.cuba.Conv(neuron_cuba_kwargs,   2,  16, 3, padding=1, stride=2, weight_scale=2, **block_8_kwargs),
+            slayer.block.cuba.Conv(neuron_cuba_kwargs,  16,  32, 3, padding=1, stride=2, weight_scale=2, **block_8_kwargs),
             slayer.block.sigma_delta.Conv(neuron_kwargs,  32,  64, 3, padding=1, stride=2, weight_scale=1, **block_8_kwargs),
             slayer.block.sigma_delta.Conv(neuron_kwargs,  64, 128, 3, padding=1, stride=2, weight_scale=3, **block_8_kwargs),
             slayer.block.sigma_delta.Conv(neuron_kwargs, 128, 256, 3, padding=1, stride=1, weight_scale=3, **block_8_kwargs),
