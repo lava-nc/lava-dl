09ad0d4739020b0c931dbe51504d02eaeb198161
y <leobardo.e.campos.macias@intel.com>
2023-12-07 13:39:23 -0800

    ds filter
    Signed-off-by: y <leobardo.e.campos.macias@intel.com>

--- tutorials/lava/lib/dl/slayer/tiny_yolo_sdnn/filter_dataset.py
+++ tutorials/lava/lib/dl/slayer/tiny_yolo_sdnn/filter_dataset.py
@@ -1,23 +1,43 @@
 import numpy as np
 from lava.lib.dl.slayer import obd
 import os
+import multiprocessing
+from joblib import Parallel, delayed
+
+
+def single_data(dataset, idx, ds_path):
+    name = dataset.get_name(idx) 
+    images, annotations = dataset[idx]
+    if not os.path.exists(ds_path + os.path.sep + name):
+        os.makedirs(ds_path + os.path.sep + name)
+        os.makedirs(ds_path + os.path.sep + name +  os.path.sep + 'events')
+        os.makedirs(ds_path + os.path.sep + name +  os.path.sep + 'labels')
+        
+    idd = 0
+    for events, label in zip(images, annotations):
+        np.savez_compressed(ds_path + os.path.sep + name +  os.path.sep + 
+                            'events' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=events)
+        np.savez_compressed(ds_path + os.path.sep + name +  os.path.sep + 
+                                'labels' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=label)
+        idd += 1
+    print(idx, '/', len(dataset), ' train_set: ', name)
+    
 
 if __name__ == '__main__':
-    train_set = obd.dataset._PropheseeAutomotive(root='/data-raid/sshresth/data/Prophesee_1mp', 
+    train_set = obd.dataset._PropheseeAutomotive(root='/home/lecampos/data/prophesee', 
                                                 delta_t = 1,
                                                 train=True, 
                                                 randomize_seq= False,
-                                                seq_len = 999999999999)
+                                                seq_len = 999999999999999)
     
-    test_set = obd.dataset._PropheseeAutomotive(root='/data-raid/sshresth/data/Prophesee_1mp', 
+    test_set = obd.dataset._PropheseeAutomotive(root='/home/lecampos/data/prophesee', 
                                                 delta_t = 1,
                                                 train=False, 
                                                 randomize_seq= False,
-                                                seq_len = 999999999999)
+                                                seq_len = 999999999999999)
                     
-
-    out_path = '/data-raid/sshresth/data/Prophesee_fl'
-
+    out_path = '/home/lecampos/data/prophesee_small'
+    
     if not os.path.exists(out_path):
         os.makedirs(out_path)
 
@@ -29,52 +49,13 @@ if __name__ == '__main__':
     if not os.path.exists(test_path):
         os.makedirs(test_path)
         
+    num_cores = multiprocessing.cpu_count() - 1
     print('starting...')
+    processed_list = Parallel(n_jobs=num_cores, prefer="threads")(delayed(single_data)(train_set, idx, train_path) for idx in range(len(train_set)))
+    processed_list = Parallel(n_jobs=num_cores, prefer="threads")(delayed(single_data)(test_set, idx, test_path) for idx in range(len(test_set)))
         
-    for idx in range(len(train_set)):
-        name = train_set.get_name(idx) 
-        images, annotations = train_set[idx]
-        if not os.path.exists(train_path + os.path.sep + name):
-            os.makedirs(train_path + os.path.sep + name)
-            os.makedirs(train_path + os.path.sep + name +  os.path.sep + 'events')
-            os.makedirs(train_path + os.path.sep + name +  os.path.sep + 'labels')
+    #for idx in range(len(train_set)):
+    #    single_data(train_set, idx, train_path)
             
-        idd = 0
-        for events, label in zip(images, annotations):
-            np.savez_compressed(train_path + os.path.sep + name +  os.path.sep + 
-                                'events' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=events)
-            np.savez_compressed(train_path + os.path.sep + name +  os.path.sep + 
-                                    'labels' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=label)
-            
-            events_loaded = np.load(train_path + os.path.sep + name +  os.path.sep + 
-                                'events' + os.path.sep + '{:05d}'.format(idd) + '.npz')['a']
-
-            label_loaded = np.load(train_path + os.path.sep + name +  os.path.sep + 
-                                'labels' + os.path.sep + '{:05d}'.format(idd) + '.npz',
-                                allow_pickle='TRUE')['a'].item()
-            idd += 1
-        print(idx, '/', len(train_set), ' train_set: ', name)
-            
-    for idx in range(len(test_set)):
-        name = test_set.get_name(idx) 
-        images, annotations = test_set[idx]
-        if not os.path.exists(test_path + os.path.sep + name):
-            os.makedirs(test_path + os.path.sep + name)
-            os.makedirs(test_path + os.path.sep + name +  os.path.sep + 'events')
-            os.makedirs(test_path + os.path.sep + name +  os.path.sep + 'labels')
-            
-        idd = 0
-        for events, label in zip(images, annotations):
-            np.savez_compressed(test_path + os.path.sep + name +  os.path.sep + 
-                                'events' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=events)
-            np.savez_compressed(test_path + os.path.sep + name +  os.path.sep + 
-                                    'labels' + os.path.sep + '{:05d}'.format(idd) + '.npz', a=label)
-            
-            events_loaded = np.load(test_path + os.path.sep + name +  os.path.sep + 
-                                'events' + os.path.sep + '{:05d}'.format(idd) + '.npz')['a']
-
-            label_loaded = np.load(test_path + os.path.sep + name +  os.path.sep + 
-                                'labels' + os.path.sep + '{:05d}'.format(idd) + '.npz',
-                                allow_pickle='TRUE')['a'].item()
-            idd += 1
-        print(idx, '/', len(test_set), ' test_set: ', name)
+    #for idx in range(len(test_set)):
+    #    single_data(test_set, idx, test_path)
