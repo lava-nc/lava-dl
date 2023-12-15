7425e0fa04c5549b12cd2da5f274727c8d563cdb
y <leobardo.e.campos.macias@intel.com>
2023-12-14 18:38:26 -0800

    CI tests
    Signed-off-by: y <leobardo.e.campos.macias@intel.com>

--- src/lava/lib/dl/slayer/object_detection/models/__init__.py
+++ src/lava/lib/dl/slayer/object_detection/models/__init__.py
@@ -2,11 +2,10 @@
 # SPDX-License-Identifier:  BSD-3-Clause
 
 from . import tiny_yolov3_str, yolo_kp
-from . import tiny_yolov3_str_events, yolo_kp_events
-from . import yolov3_ann, tiny_yolov3_ann
+from . import yolo_kp_events
+
 
 __all__ = [
-    'tiny_yolov3_str', 'yolo_kp', 
-    'tiny_yolov3_str_events', 'yolo_kp_events'
-    'yolov3_ann', 'tiny_yolov3_ann'
+    'tiny_yolov3_str', 'yolo_kp',
+    'yolo_kp_events'
 ]
