fixing graded surrogate gradient sign (#286)
Co-authored-by: chcaccac <caterina.caccavella@sony.com>

src/lava/lib/dl/slayer/spike/spike.py
@@ -19,7 +19,7 @@ def _spike_backward(
         )
 
     return scale_rho * torch.exp(
-        -torch.clamp(voltage - threshold, max=0) / tau_rho
+        torch.clamp(voltage - threshold, max=0) / tau_rho
     )
 
 