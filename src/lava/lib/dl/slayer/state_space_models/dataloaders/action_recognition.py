862fb3a61a2518fcd3a598665f99a7a89f9cbb21
Philipp Weidel <philipp.weidel@intel.com>
2024-05-28 15:24:22 +0000

    dataloader for ntu effnet

---
+++ src/lava/lib/dl/slayer/state_space_models/dataloaders/action_recognition.py
@@ -0,0 +1,359 @@
+import os
+import os.path
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+import torch
+from typing import List, Union, Tuple, Any
+import time
+from torch.utils.data import WeightedRandomSampler
+
+class VideoRecord(object):
+    """
+    Helper class for class VideoFrameDataset. This class
+    represents a video sample's metadata.
+
+    Args:
+        root_datapath: the system path to the root folder
+                       of the videos.
+        row: A list with four or more elements where 1) The first
+             element is the path to the video sample's frames excluding
+             the root_datapath prefix 2) The  second element is the starting frame id of the video
+             3) The third element is the inclusive ending frame id of the video
+             4) The fourth element is the label index.
+             5) any following elements are labels in the case of multi-label classification
+    """
+    def __init__(self, row, root_datapath):
+        self._data = row
+        self._path = os.path.join(root_datapath, row[0])
+
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def num_frames(self) -> int:
+        return self.end_frame - self.start_frame + 1  # +1 because end frame is inclusive
+    @property
+    def start_frame(self) -> int:
+        return int(self._data[1])
+
+    @property
+    def end_frame(self) -> int:
+        return int(self._data[2])
+
+    @property
+    def label(self) -> Union[int, List[int]]:
+        # just one label_id
+        if len(self._data) == 4:
+            return int(self._data[3])
+        # sample associated with multiple labels
+        else:
+            return [int(label_id) for label_id in self._data[3:]]
+
+class NTUEfficientNetDataset(torch.utils.data.Dataset):
+    r"""
+    A highly efficient and adaptable dataset class for videos.
+    Instead of loading every frame of a video,
+    loads x RGB frames of a video (sparse temporal sampling) and evenly
+    chooses those frames from start to end of the video, returning
+    a list of x PIL images or ``FRAMES x CHANNELS x HEIGHT x WIDTH``
+    tensors where FRAMES=x if the ``ImglistToTensor()``
+    transform is used.
+
+    More specifically, the frame range [START_FRAME, END_FRAME] is divided into NUM_SEGMENTS
+    segments and FRAMES_PER_SEGMENT consecutive frames are taken from each segment.
+
+    Note:
+        A demonstration of using this class can be seen
+        in ``demo.py``
+        https://github.com/RaivoKoot/Video-Dataset-Loading-Pytorch
+
+    Note:
+        This dataset broadly corresponds to the frame sampling technique
+        introduced in ``Temporal Segment Networks`` at ECCV2016
+        https://arxiv.org/abs/1608.00859.
+
+
+    Note:
+        This class relies on receiving video data in a structure where
+        inside a ``ROOT_DATA`` folder, each video lies in its own folder,
+        where each video folder contains the frames of the video as
+        individual files with a naming convention such as
+        img_001.jpg ... img_059.jpg.
+        For enumeration and annotations, this class expects to receive
+        the path to a .txt file where each video sample has a row with four
+        (or more in the case of multi-label, see README on Github)
+        space separated values:
+        ``VIDEO_FOLDER_PATH     START_FRAME      END_FRAME      LABEL_INDEX``.
+        ``VIDEO_FOLDER_PATH`` is expected to be the path of a video folder
+        excluding the ``ROOT_DATA`` prefix. For example, ``ROOT_DATA`` might
+        be ``home\data\datasetxyz\videos\``, inside of which a ``VIDEO_FOLDER_PATH``
+        might be ``jumping\0052\`` or ``sample1\`` or ``00053\``.
+
+    Args:
+        root_path: The root path in which video folders lie.
+                   this is ROOT_DATA from the description above.
+        annotationfile_path: The .txt annotation file containing
+                             one row per video sample as described above.
+        num_segments: The number of segments the video should
+                      be divided into to sample frames from.
+        frames_per_segment: The number of frames that should
+                            be loaded per segment. For each segment's
+                            frame-range, a random start index or the
+                            center is chosen, from which frames_per_segment
+                            consecutive frames are loaded.
+        imagefile_template: The image filename template that video frame files
+                            have inside of their video folders as described above.
+        transform: Transform pipeline that receives a list of PIL images/frames.
+        test_mode: If True, frames are taken from the center of each
+                   segment, instead of a random location in each segment.
+
+    """
+    def __init__(self,
+                 root_path: str,
+                 annotationfile_path: str,
+                 num_segments: int = 30,
+                 frames_per_segment: int = 1,
+                 imagefile_template: str='img_{:05d}.png',
+                 transform = None,
+                 test_mode: bool = False,
+                 classify_labels: list = []):
+        super().__init__()
+
+        self.root_path = root_path
+        self.annotationfile_path = annotationfile_path
+        self.num_segments = num_segments
+        self.frames_per_segment = frames_per_segment
+        self.imagefile_template = imagefile_template
+        self.transform = transform
+        self.test_mode = test_mode
+        self.label_list = []
+        self.classify_labels = classify_labels
+        if self.classify_labels == 'all':
+            self.label_map = {i: i for i in range(120)}
+        else:
+            self.label_map = {v: i+1 for i, v in enumerate(classify_labels)}
+
+        self._parse_annotationfile()
+        self._sanity_check_samples()
+ 
+    @property
+    def num_classes(self):
+        if self.classify_labels == 'all':
+            return len(self.label_map)
+        else:
+            # When we specify a subset of labels, the 0 class implicitly includes the remaining classes
+            return len(self.label_map) + 1
+
+
+    def balance_by_random_drop(self, force_min=None):
+        """
+        Balances the dataset by dropping random samples.
+        """
+
+        class_counts = np.bincount(self.label_list)
+        if force_min:
+            min_class_count = force_min
+        else:
+            min_class_count = np.min(class_counts)
+
+        # num_samples_to_drop = class_counts - min_class_count
+        num_samples_to_drop = [np.max([0, cc - min_class_count]) for cc in class_counts]
+
+        for class_id, to_drop in enumerate(num_samples_to_drop):
+            ids = np.where(np.array(self.label_list) == class_id)[0]
+            ids_to_drop = np.random.choice(ids, to_drop, replace=False)
+            self.label_list = [label for id, label in enumerate(self.label_list) if id not in ids_to_drop]
+            self.video_list = [label for id, label in enumerate(self.video_list) if id not in ids_to_drop]
+
+    def _load_image(self, base_path: str, frame_index: list) -> Image.Image:
+        data = np.load(base_path)
+
+        return data[np.array(frame_index)]
+
+
+
+    def _parse_annotationfile(self):
+        self.video_list = [VideoRecord(x.strip().split(), self.root_path) for x in open(self.annotationfile_path)]
+        self.label_list = [self.label_map.get(record.label, 0) for record in self.video_list]
+
+    def _sanity_check_samples(self):
+        warning_idx = []
+        for i, record in enumerate(self.video_list):
+            if record.num_frames <= 0 or record.start_frame == record.end_frame:
+                warning_idx.append(i)
+                #print(f"\nDataset Warning: video {record.path} seems to have zero RGB frames on disk!\n")
+
+            elif record.num_frames < (self.num_segments * self.frames_per_segment):
+                warning_idx.append(i)
+                #print(f"\nDataset Warning: video {record.path} has {record.num_frames} frames "
+                #      f"but the dataloader is set up to load "
+                #      f"(num_segments={self.num_segments})*(frames_per_segment={self.frames_per_segment})"
+                #      f"={self.num_segments * self.frames_per_segment} frames. Dataloader will repeat the "
+                #      f"first frame when trying to load this video.\n")
+        print(f"encountered {len(warning_idx)=}. Deleting those from file list.")
+
+        for i in reversed(warning_idx):
+            del self.video_list[i]
+            del self.label_list[i]
+        print("num labels after removal:", len(self.video_list), len(self.label_list))
+
+
+    def _get_start_indices(self, record: VideoRecord) -> 'np.ndarray[int]':
+        """
+        For each segment, choose a start index from where frames
+        are to be loaded from.
+
+        Args:
+            record: VideoRecord denoting a video sample.
+        Returns:
+            List of indices of where the frames of each
+            segment are to be loaded from.
+        """
+        num_frames_needed = self.num_segments * self.frames_per_segment
+        if record.num_frames < num_frames_needed:
+            # return all frames and repeat the first one
+            start_indices = (num_frames_needed - record.num_frames) * [record.start_frame] + list(range(record.start_frame, record.end_frame + 1))
+            return start_indices
+        # choose start indices that are perfectly evenly spread across the video frames.
+        if self.test_mode:
+            distance_between_indices = (record.num_frames - self.frames_per_segment + 1) / float(self.num_segments)
+
+            start_indices = np.array([int(distance_between_indices / 2.0 + distance_between_indices * x)
+                                      for x in range(self.num_segments)])
+        # randomly sample start indices that are approximately evenly spread across the video frames.
+        else:
+            max_valid_start_index = (record.num_frames - self.frames_per_segment + 1) // self.num_segments
+
+            start_indices = np.multiply(list(range(self.num_segments)), max_valid_start_index) + \
+                      np.random.randint(max_valid_start_index, size=self.num_segments)
+
+        return start_indices + record.start_frame
+
+    def __getitem__(self, idx: int) -> Union[
+        Tuple[List[Image.Image], Union[int, List[int]]],
+        Tuple['torch.Tensor[num_frames, channels, height, width]', Union[int, List[int]]],
+        Tuple[Any, Union[int, List[int]]],
+        ]:
+        """
+        For video with id idx, loads self.NUM_SEGMENTS * self.FRAMES_PER_SEGMENT
+        frames from evenly chosen locations across the video.
+
+        Args:
+            idx: Video sample index.
+        Returns:
+            A tuple of (video, label). Label is either a single
+            integer or a list of integers in the case of multiple labels.
+            Video is either 1) a list of PIL images if no transform is used
+            2) a batch of shape (NUM_IMAGES x CHANNELS x HEIGHT x WIDTH) in the range [0,1]
+            if the transform "ImglistToTensor" is used
+            3) or anything else if a custom transform is used.
+        """
+        record: VideoRecord = self.video_list[idx]
+        label = self.label_list[idx]
+
+        frame_start_indices: 'np.ndarray[int]' = self._get_start_indices(record)
+
+        return self._get(record, frame_start_indices), label
+
+    def _get(self, record: VideoRecord, frame_start_indices: 'np.ndarray[int]') -> Union[
+        Tuple[List[Image.Image], Union[int, List[int]]],
+        Tuple['torch.Tensor[num_frames, channels, height, width]', Union[int, List[int]]],
+        Tuple[Any, Union[int, List[int]]],
+        ]:
+        """
+        Loads the frames of a video at the corresponding
+        indices.
+
+        Args:
+            record: VideoRecord denoting a video sample.
+            frame_start_indices: Indices from which to load consecutive frames from.
+        Returns:
+            A tuple of (video, label). Label is either a single
+            integer or a list of integers in the case of multiple labels.
+            Video is either 1) a list of PIL images if no transform is used
+            2) a batch of shape (NUM_IMAGES x CHANNELS x HEIGHT x WIDTH) in the range [0,1]
+            if the transform "ImglistToTensor" is used
+            3) or anything else if a custom transform is used.
+        """
+
+        # frame_start_indices = np.array(frame_start_indices + record.start_frame)
+
+        images = []
+        # from each start_index, load self.frames_per_segment
+        # consecutive frames
+
+        frame_idx = []
+        for idx in frame_start_indices:
+            idx = int(idx)
+            frame_idx.append(idx)
+
+            # load self.frames_per_segment consecutive frames
+            for _ in range(self.frames_per_segment - 1):
+
+                if idx < record.end_frame:
+                    idx += 1
+                    frame_idx.append(idx)
+
+
+        images = self._load_image(record.path, frame_idx) 
+
+        return images
+
+    def __len__(self):
+        return len(self.video_list)
+
+def init_effnet_NTU_dataloader(partition="train",
+                               batch_size=8,
+                               num_frames_per_sample=30,
+                               data_root=None):
+    # videos_root = os.path.join("/ssd2/users/pweidel/datasets/NTU/data_frames/")
+    annotation_file = os.path.join(data_root, partition + '_effnet_features.txt')
+
+    # classify_labels = [5, 6, 8, 9, 14, 15, 43, 51, 23, 27, 48]
+    # classify_labels = [41, 42, 43, 44, 45, 46, 47, 48, 104]
+    classify_labels = 'all'
+    
+    preprocess = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    
+    dataset = NTUEfficientNetDataset(
+        root_path="",
+        annotationfile_path=annotation_file,
+        num_segments=num_frames_per_sample,
+        frames_per_segment=1,
+        transform=preprocess,
+        test_mode=False if partition == "train" else True,
+        classify_labels = classify_labels 
+    )
+
+    if not partition == 'train':
+        dataset.balance_by_random_drop()
+    
+    sample_labels = dataset.label_list # list/array of labels
+    class_counts = np.bincount(sample_labels)
+    num_classes = len(class_counts)
+    samples_per_class = np.min(class_counts)
+    class_weights = 1. / class_counts   # assuming labels are class indices
+    sample_weights = class_weights[sample_labels]
+
+    
+    # sampler = WeightedRandomSampler(sample_weights, num_samples=len(sample_weights))
+    sampler = WeightedRandomSampler(sample_weights,
+                                    num_samples=int(samples_per_class * num_classes),
+                                    replacement=True)
+
+    
+    dataloader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=max([batch_size, 12]),
+        pin_memory=True,
+        sampler=sampler if partition == 'train' else None,
+        shuffle=False if partition == 'train' else True,
+    )
+
+    return dataloader
