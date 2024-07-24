ae973e746c5c81b2a58d6e6142d860665e86ffd2
Michaeljurado42 <michaeljurado42@gmail.com>
2024-07-23 21:59:45 -0400

    Fix weight_exp is None for netx DelaySynapse  (#316)
    * Added weight_exp argument to DelayDenseSynapse
    
    * Fix small linting error
    
    ---------
    
    Co-authored-by: bamsumit <bam_sumit@hotmail.com>

--- src/lava/lib/dl/netx/blocks/process.py
+++ src/lava/lib/dl/netx/blocks/process.py
@@ -173,9 +173,11 @@ class Dense(AbstractBlock):
             self.synapse = Synapse(
                 weights=weight,
                 delays=delay.astype(int),
+                weight_exp=weight_exponent,
                 max_delay=62,
                 num_weight_bits=num_weight_bits,
                 num_message_bits=self.input_message_bits,
+
             )
 
         if self.shape != self.synapse.a_out.shape:
